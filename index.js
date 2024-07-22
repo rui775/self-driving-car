cc3ceff866418749f3c06c9f02b4fd002e215655
rui775 <rsilva6420.dev@gmail.com>
2024-07-22 15:34:00 +0100

    Do Respond Events tasks.

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const computer_vision_1 = require("./computer-vision");
 class Car {
     constructor(props) {
         this.isRunning = props.isRunning;
     }
+    respond(events) {
+        if (!this.isRunning) {
+            console.log('The car is off...');
+        }
+    }
 }
-const autonomousCar = new Car({ isRunning: true });
-console.log(autonomousCar.isRunning);
+const autonomousCar = new Car({ isRunning: false });
+// console.log(autonomousCar.isRunning);
+autonomousCar.respond((0, computer_vision_1.getObstacleEvents)());
--- index.ts
+++ index.ts
@@ -1,11 +1,16 @@
 import { getObstacleEvents } from './computer-vision';
 
 interface AutonomousCar {
-    isRunning: boolean;
+    isRunning?: boolean;
+    respond: (events: Events) => void;
 }
 
 interface AutonomousCarProps {
-    isRunning: boolean;
+    isRunning?: boolean;
+}
+
+interface Events {
+    [event: string]: boolean;
 }
 
 class Car implements AutonomousCar {
@@ -13,8 +18,15 @@ class Car implements AutonomousCar {
     constructor(props: AutonomousCarProps) {
         this.isRunning = props.isRunning;
     }
+
+    respond(events: Events) {
+        if (!this.isRunning) {
+            console.log('The car is off...')
+        }
+    }
 }
 
-const autonomousCar = new Car({ isRunning: true });
+const autonomousCar = new Car({ isRunning: false });
 
-console.log(autonomousCar.isRunning);
+// console.log(autonomousCar.isRunning);
+autonomousCar.respond(getObstacleEvents());
